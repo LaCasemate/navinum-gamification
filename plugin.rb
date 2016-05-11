01af06fb4af4577385357108bdf814f976fa52c6
Nicolas Florentin <nicolas@sleede.com>
2016-05-11 12:30:25 +0200

    adds views for notifications mailer and proof of concept of adding a notification

--- plugin.rb
+++ plugin.rb
@@ -13,4 +13,17 @@ after_initialize do
     end
   end
 
+  NotificationsMailer.class_eval do
+    append_view_path "#{Rails.root}/plugins/navi_gami/views"
+
+    def notify_navi_gami_test # dummy code but proof of concept
+      @notification = Notification.last
+      mail(to: 'test@sleede.com')  # will search for notify_navi_gami_test.html
+                                  # will search subject in notifications_mailer.notify_navi_gami_test.subject in translations
+    end
+  end
+
+  NotificationType.class_eval do
+    notification_type_names %w(notify_navi_gami_test)
+  end
 end
