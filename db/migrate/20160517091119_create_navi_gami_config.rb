c86757441e9eeb0ec9cf6e75497d3049a858cc95
Nicolas Florentin <nicolas@sleede.com>
2016-05-18 11:24:38 +0200

    removes eventually useless attr

--- db/migrate/20160517091119_create_navi_gami_config.rb
+++ db/migrate/20160517091119_create_navi_gami_config.rb
@@ -3,7 +3,6 @@ class CreateNaviGamiConfig < ActiveRecord::Migration
     create_table :navi_gami_configs do |t|
       t.text :external_space_url
       t.text :api_url
-      t.string :profile_background_img
     end
   end
 end
